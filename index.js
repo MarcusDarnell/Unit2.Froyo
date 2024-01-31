b281116184a31604ca91bd997c9ec92b8edfe82a
MarcusDarnell <mdarnell12894@gmail.com>
2024-01-31 08:31:32 -0800

    first commit

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
-let froyoOrder = prompt('Please enter your order below!' )
+let froyoOrder = prompt('Please enter your order below seperated by commas!','vanilla,vanilla,vanilla,strawberry,coffee,coffee');
+
+let onlineOrder = froyoOrder.split(",");
+
+console.log(onlineOrder);
+
+function froyo (flavorsOrder) {
+    const Howmanyflavors = {};
+    for (let i=0; i < flavorsOrder.length; i++) {
+    let currentFlavors = flavorsOrder[i];
+
+        if (Howmanyflavors [currentFlavors] === undefined) {
+
+            Howmanyflavors[currentFlavors] = 1;
+       }
+       else {Howmanyflavors[currentFlavors] += 1}
+   } return Howmanyflavors
+}
+console.table(froyo(onlineOrder))
 
